df66fc72dc5e52011a7eab31b19595234d8efdd8
uncertify <uncertifieddevice@gmail.com>
2019-11-06 01:31:32 +0000

    Implement wackButton class
    Button control class that keeps track of a single button to be used in a loop, doesn't rely on interrupts, also implements some simple comparisons to interpret button changes.

--- wackbutton.h
+++ wackbutton.h
@@ -4,11 +4,42 @@
 class wackButton
 {
   public:
-    wackButton() {}
+    //Constructor sets the pin used for the button and changes its mode to INPUT
+    wackButton(short int _pin)
+    : pin(_pin) 
+    {
+      pinMode(pin, INPUT);
+    }
+
+    //The update() function reads the state of the button and updates attributes accordingly
+    void update() 
+    {
+      prevState = currState;
+      currState = digitalRead(pin);
+    }
+
+    //Note: In our circuit the button gives out a HIGH signal while pressed down
+
+    //Check if the button has been released this update() call
+    bool up() { return prevState == HIGH && currState == LOW; }
+
+    //Check if the button has been pressed this update() call
+    bool down() { return prevState == LOW && currState == HIGH; }
+
+    //Check if the button is currently pressed
+    bool high() { return currState == HIGH; }
+
+    //Blank destructor
     virtual ~wackButton() {}
 
   private;
-    //
+    //Pin to read for button input
+    short int pin;
+
+    //State of the button since the last update() call
+    bool currState = LOW;
+    //State of the button since  update() call before
+    bool prevState = LOW;
 }
 
 #endif
